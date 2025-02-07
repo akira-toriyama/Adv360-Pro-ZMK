f211296be52207df6b422f299745fc6bb315b3bf
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2025-02-07 12:10:49 +0900

    Fix mouse setting

--- _/build.sh
+++ _/build.sh
@@ -4,7 +4,7 @@
 dtsfmt ./config/adv360.keymap
 
 # 左
-rm -rf ./firmware/*.uf2 || true && make && cp ./firmware/*-left.uf2  /Volumes/ADV360PRO/.
+rm -rf ./firmware/*.uf2 || true && make && cp ./firmware/*-left-*.uf2  /Volumes/ADV360PRO/.
 
 # 右
-# rm -rf ./firmware/*.uf2 || true && make && cp ./firmware/*-right.uf2  /Volumes/ADV360PRO/.
+# rm -rf ./firmware/*.uf2 || true && make && cp ./firmware/*-right-*.uf2  /Volumes/ADV360PRO/.
